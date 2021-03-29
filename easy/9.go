19db2a400864a3350943982e00b172f80399d294
lisa.li <lisa.x.li@sap.com>
2021-03-29 19:26:20 +0800

    9

---
+++ easy/9.go
@@ -0,0 +1,16 @@
+package main
+
+import "strconv"
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	s := strconv.Itoa(x)
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
