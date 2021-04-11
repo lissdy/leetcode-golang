635674f1ec20b473b63e41c58664cd6b8ff09aa1
lisa.li <lisa.x.li@sap.com>
2021-04-11 16:47:45 +0800

    264

---
+++ medium/264.go
@@ -0,0 +1,62 @@
+package main
+
+//给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+//
+// 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：n = 10
+//输出：12
+//解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+//
+//
+// 示例 2：
+//
+//
+//输入：n = 1
+//输出：1
+//解释：1 通常被视为丑数。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= n <= 1690
+//
+// Related Topics 堆 数学 动态规划
+// 👍 525 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func nthUglyNumber(n int) int {
+	dp := make([]int, n+1)
+	dp[1] = 1
+	p2, p3, p5 := 1, 1, 1
+	for i := 2; i <= n; i++ {
+		dp[i] = min(min(dp[p2]*2, dp[p3]*3), dp[p5]*5)
+		if dp[i] == dp[p2]*2 {
+			p2++
+		}
+		if dp[i] == dp[p3]*3 {
+			p3++
+		}
+		if dp[i] == dp[p5]*5 {
+			p5++
+		}
+	}
+	return dp[n]
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
