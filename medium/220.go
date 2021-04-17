884ec2273b69a155d7a51617ae7545ff2824d149
lisa.li <lisa.x.li@sap.com>
2021-04-17 22:05:14 +0800

    220

---
+++ medium/220.go
@@ -0,0 +1,61 @@
+package main
+
+//给你一个整数数组 nums 和两个整数 k 和 t 。请你判断是否存在 两个不同下标 i 和 j，使得 abs(nums[i] - nums[j]) <=
+//t ，同时又满足 abs(i - j) <= k 。
+//
+// 如果存在则返回 true，不存在返回 false。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,2,3,1], k = 3, t = 0
+//输出：true
+//
+// 示例 2：
+//
+//
+//输入：nums = [1,0,1,1], k = 1, t = 2
+//输出：true
+//
+// 示例 3：
+//
+//
+//输入：nums = [1,5,9,1,5,9], k = 2, t = 3
+//输出：false
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= nums.length <= 2 * 104
+// -231 <= nums[i] <= 231 - 1
+// 0 <= k <= 104
+// 0 <= t <= 231 - 1
+//
+// Related Topics 排序 Ordered Map
+// 👍 410 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func containsNearbyAlmostDuplicate(nums []int, k int, t int) bool {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j <= min(i+k, len(nums)-1); j++ {
+			if abs(nums[i], nums[j]) <= t {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func abs(a int, b int) int {
+	ans := a - b
+	if ans < 0 {
+		return 0 - ans
+	}
+	return ans
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
